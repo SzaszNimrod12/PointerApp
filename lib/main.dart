eed4a81d7914a2160c82ce557370a2bd01f157e8
SzaszNimrod12 <szasz.nimrod.janos@student.ms.sapientia.ro>
2022-11-01 18:59:05 +0200

    frissites kijavitva

---
+++ lib/main.dart
@@ -0,0 +1,215 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:web_socket_channel/web_socket_channel.dart';
+import 'package:flutter/material.dart';
+import 'package:sensors_plus/sensors_plus.dart';
+import 'dart:async';
+
+void main() async => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const title = 'WebSocket Demo';
+    return const MaterialApp(
+      title: title,
+      home: MyHomePage(
+        title: title,
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final TextEditingController _controller = TextEditingController();
+  final _channel = WebSocketChannel.connect(
+    Uri.parse('ws://192.168.0.154:8080'),
+  );
+  List<double>? _accelerometerValues;
+  List<double>? _userAccelerometerValues;
+  List<double>? _gyroscopeValues;
+  List<double>? _magnetometerValues;
+  final _streamSubscriptions = <StreamSubscription<dynamic>>[];
+  dynamic isPressed;
+  dynamic response;
+  dynamic responsetest;
+
+  @override
+  Widget build(BuildContext context) {
+    final accelerometer =
+        _accelerometerValues?.map((double v) => v.toStringAsFixed(1)).toList();
+    final gyroscope =
+        _gyroscopeValues?.map((double v) => v.toStringAsFixed(1)).toList();
+    final userAccelerometer = _userAccelerometerValues
+        ?.map((double v) => v.toStringAsFixed(1))
+        .toList();
+    final magnetometer =
+        _magnetometerValues?.map((double v) => v.toStringAsFixed(1)).toList();
+
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 24),
+            StreamBuilder(
+              stream: _channel.stream,
+              builder: (context, snapshot) {
+                return Text(snapshot.hasData ? '${snapshot.data}' : '');
+              },
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text('Accelerometer: $accelerometer'),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text('UserAccelerometer: $userAccelerometer'),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text('Gyroscope: $gyroscope'),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text('Magnetometer: $magnetometer'),
+                ],
+              ),
+            ),
+            GestureDetector(
+              onLongPressStart: (_) async {
+                _sendMessageCalibrate();
+                isPressed = true;
+                do {
+                  //print('long pressing');
+                  _sendMessage();
+                  //testeles milyat 1 masodperc kesleltetes van de lehet ez sokall kisebb.
+                  await Future.delayed(Duration(seconds: 1));
+                } while (isPressed);
+              },
+              child: Container(
+                color: Colors.blueAccent,
+                padding: const EdgeInsets.all(12),
+                child: const Text("Send"),
+              ),
+              onLongPressEnd: (_) => setState(() => isPressed = false),
+            ),
+
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _sendMessageStop,
+        tooltip: 'Send server stop message',
+        child: const Icon(Icons.stop_screen_share_outlined),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    lisenSensorData();
+    super.initState();
+  }
+
+  void _sendMessage() {
+    //_channel.sink.add(responsetest);
+    _channel.sink.add(response);
+  }
+  void _sendMessageStop(){
+    _channel.sink.add('stop');
+  }
+  void _sendMessageCalibrate(){
+    _channel.sink.add('calibrate');
+  }
+
+
+  void lisenSensorData() {
+    _streamSubscriptions.add(
+      accelerometerEvents.listen(
+        (AccelerometerEvent event) {
+          setState(() {
+            _accelerometerValues = <double>[event.x, event.y, event.z];
+          });
+        },
+      ),
+    );
+    _streamSubscriptions.add(
+      gyroscopeEvents.listen(
+        (GyroscopeEvent event) {
+          setState(() {
+            _gyroscopeValues = <double>[event.x, event.y, event.z];
+          });
+        },
+      ),
+    );
+    _streamSubscriptions.add(
+      userAccelerometerEvents.listen(
+        (UserAccelerometerEvent event) {
+          setState(() {
+            _userAccelerometerValues = <double>[event.x, event.y, event.z];
+            response=json.encode({'x':event.x,'y':event.y,'z':event.z});
+          });
+        },
+      ),
+    );
+    _streamSubscriptions.add(
+      magnetometerEvents.listen(
+        (MagnetometerEvent event) {
+          setState(() {
+            _magnetometerValues = <double>[event.x, event.y, event.z];
+            responsetest=json.encode({'x':event.x,'y':event.y,'z':event.z});
+
+          });
+        },
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _channel.sink.close();
+    _controller.dispose();
+    super.dispose();
+    for (final subscription in _streamSubscriptions) {
+      subscription.cancel();
+    }
+  }
+}
