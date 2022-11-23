3ceec0f24618b948d2d96beb91ac6651a14a5da8
SzaszNimrod12 <szasz.nimrod.janos@student.ms.sapientia.ro>
2022-11-23 16:57:01 +0200

    rajzolas implementalva

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const title = 'WebSocket Demo';
-    return const MaterialApp(
+    const title = 'Pointer App';
+    return MaterialApp(
       title: title,
+      theme: ThemeData(
+        brightness: Brightness.dark),
       home: MyHomePage(
         title: title,
       ),
@@ -40,26 +42,14 @@ class _MyHomePageState extends State<MyHomePage> {
     Uri.parse('ws://192.168.0.154:8080'),
   );
   List<double>? _accelerometerValues;
-  List<double>? _userAccelerometerValues;
-  List<double>? _gyroscopeValues;
-  List<double>? _magnetometerValues;
   final _streamSubscriptions = <StreamSubscription<dynamic>>[];
   dynamic isPressed;
   dynamic response;
-  dynamic responsetest;
 
   @override
   Widget build(BuildContext context) {
     final accelerometer =
         _accelerometerValues?.map((double v) => v.toStringAsFixed(1)).toList();
-    final gyroscope =
-        _gyroscopeValues?.map((double v) => v.toStringAsFixed(1)).toList();
-    final userAccelerometer = _userAccelerometerValues
-        ?.map((double v) => v.toStringAsFixed(1))
-        .toList();
-    final magnetometer =
-        _magnetometerValues?.map((double v) => v.toStringAsFixed(1)).toList();
-
 
     return Scaffold(
       appBar: AppBar(
@@ -68,7 +58,7 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             const SizedBox(height: 24),
             StreamBuilder(
@@ -78,7 +68,7 @@ class _MyHomePageState extends State<MyHomePage> {
               },
             ),
             Padding(
-              padding: const EdgeInsets.all(16.0),
+              padding: const EdgeInsets.all(5.0),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
@@ -87,57 +77,64 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ),
             Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Text('UserAccelerometer: $userAccelerometer'),
-                ],
+              padding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
+              child: GestureDetector(
+                onLongPressStart: (_) async {
+                  _sendMessageCalibrate();
+                  _sendMessageStartLaserPointer();
+                  isPressed = true;
+                  do {
+                    //print('long pressing');
+                    _sendMessage();
+                    //testeles milyat 1 masodperc kesleltetes van de lehet ez sokall kisebb.
+                    await Future.delayed(const Duration(milliseconds: 100));
+                  } while (isPressed);
+                },
+                child: Container(
+                  width: 150,
+                  height: 150,
+                  color: Colors.blue,
+                  padding: const EdgeInsets.all(12),
+                  child: const Center(child: Text("Laser Pointer")),
+                ),
+                onLongPressEnd: (_){
+                  _sendMessageStopLaserPointer();
+                  setState(() => isPressed = false);
+                }
               ),
             ),
             Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Text('Gyroscope: $gyroscope'),
-                ],
+              padding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
+              child: GestureDetector(
+                  onLongPressStart: (_) async {
+                    _sendMessageCalibrate();
+                    _sendMessageStartDraw();
+                    isPressed = true;
+                    do {
+                      _sendMessage();
+                      await Future.delayed(const Duration(milliseconds: 100));
+                    } while (isPressed);
+                  },
+                  child: Container(
+                    width: 150,
+                    height: 150,
+                    color: Colors.blue,
+                    padding: const EdgeInsets.all(12),
+                    child: const Center(child: Text("Draw")),
+                  ),
+                  onLongPressEnd: (_){
+                    _sendMessageStopDraw();
+                    setState(() => isPressed = false);
+                  }
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Text('Magnetometer: $magnetometer'),
-                ],
-              ),
-            ),
-            GestureDetector(
-              onLongPressStart: (_) async {
-                _sendMessageCalibrate();
-                isPressed = true;
-                do {
-                  //print('long pressing');
-                  _sendMessage();
-                  //testeles milyat 1 masodperc kesleltetes van de lehet ez sokall kisebb.
-                  await Future.delayed(const Duration(milliseconds: 100));
-                } while (isPressed);
-              },
-              child: Container(
-                color: Colors.blueAccent,
-                padding: const EdgeInsets.all(12),
-                child: const Text("Send"),
-              ),
-              onLongPressEnd: (_) => setState(() => isPressed = false),
-            ),
-
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _sendMessageStop,
         tooltip: 'Send server stop message',
+        backgroundColor: Colors.blue,
         child: const Icon(Icons.stop_screen_share_outlined),
       ),
     );
@@ -150,16 +147,32 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void _sendMessage() {
-    //_channel.sink.add(responsetest);
     _channel.sink.add(response);
   }
   void _sendMessageStop(){
-    _channel.sink.add('stop');
+    var responseAction=json.encode({'type':'actions','action':'stop'});
+    _channel.sink.add(responseAction);
   }
   void _sendMessageCalibrate(){
-    _channel.sink.add('calibrate');
+    var responseAction=json.encode({'type':'actions','action':'calibrate'});
+    _channel.sink.add(responseAction);
   }
-
+  void _sendMessageStopLaserPointer(){
+    var responseAction=json.encode({'type':'actions','action':'stopLaserPointer'});
+    _channel.sink.add(responseAction);
+  }
+  void _sendMessageStartLaserPointer(){
+    var responseAction=json.encode({'type':'actions','action':'startLaserPointer'});
+    _channel.sink.add(responseAction);
+  }
+  void _sendMessageStopDraw(){
+    var responseAction=json.encode({'type':'actions','action':'stopDraw'});
+    _channel.sink.add(responseAction);
+  }
+  void _sendMessageStartDraw(){
+    var responseAction=json.encode({'type':'actions','action':'startDraw'});
+    _channel.sink.add(responseAction);
+  }
 
   void lisenSensorData() {
     _streamSubscriptions.add(
@@ -167,36 +180,7 @@ class _MyHomePageState extends State<MyHomePage> {
         (AccelerometerEvent event) {
           setState(() {
             _accelerometerValues = <double>[event.x, event.y, event.z];
-          });
-        },
-      ),
-    );
-    _streamSubscriptions.add(
-      gyroscopeEvents.listen(
-        (GyroscopeEvent event) {
-          setState(() {
-            _gyroscopeValues = <double>[event.x, event.y, event.z];
-          });
-        },
-      ),
-    );
-    _streamSubscriptions.add(
-      userAccelerometerEvents.listen(
-        (UserAccelerometerEvent event) {
-          setState(() {
-            _userAccelerometerValues = <double>[event.x, event.y, event.z];
-            response=json.encode({'x':event.x,'y':event.y,'z':event.z});
-          });
-        },
-      ),
-    );
-    _streamSubscriptions.add(
-      magnetometerEvents.listen(
-        (MagnetometerEvent event) {
-          setState(() {
-            _magnetometerValues = <double>[event.x, event.y, event.z];
-            responsetest=json.encode({'x':event.x,'y':event.y,'z':event.z});
-
+            response=json.encode({'type':'points','x':event.x,'y':event.y,'z':event.z});
           });
         },
       ),
